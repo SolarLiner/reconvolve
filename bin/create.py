00beedaaf6ba363183ce0c12a6cb2253cbae2c63
Nathan Graule <solarliner@gmail.com>
2019-04-09 15:41:04 +0200

    Don't write JSON files anymore, and use 20 to Nyquist frequency for sweep gen

--- bin/create.py
+++ bin/create.py
@@ -12,15 +12,15 @@ class CreateCommand(BaseCommand):
     description = "Create the sweep signal to be used for the recording of the response"
     def setup(self, parser: ArgumentParser):
         parser.add_argument("file", help="Output wav file. Will also output metadata json next to it.")
-        parser.add_argument("w1", type=float, default=20.0, nargs='?', help="Base frequency")
-        parser.add_argument("w2", type=float, default=20e3, nargs='?', help="Target frequency")
         parser.add_argument("-l", "--length", type=float, nargs='?', default=2.0, help="Length of the input signal")
-        parser.add_argument("-a", "--amplitude", type=float, nargs='?', default=1.0, help="Amplitude of the input signal")
+        parser.add_argument("-a", "--amplitude", type=float, nargs='?', default=0.7, help="Amplitude of the input signal")
         parser.add_argument("--samplerate", type=int, default=int(48e3), help="Sample rate (default 48000)")
 
     def run(self, args: Namespace):
         logger.debug(repr(args.__dict__))
-        p = Probe(args.w1, args.w2)
+        w1 = 20
+        w2 = args.samplerate / 2.0
+        p = Probe(w1, w2)
         p.sample_rate = args.samplerate
         array = p.generate(args.amplitude, args.length)
         p.write(args.file, array)
--- bin/process.py
+++ bin/process.py
@@ -24,23 +24,19 @@ class ProcessCommand(BaseCommand):
         parser.add_argument("output",
                             type=FileType("wb"),
                             help="Output impulse response")
-        parser.add_argument("w1",
-                            nargs='?',
-                            default=20,
-                            type=float,
-                            help="Start frequency")
-        parser.add_argument("w2",
-                            nargs='?',
-                            default=20e3,
-                            type=float,
-                            help="End frequency")
 
     def run(self, args: Namespace):
         sweep_rate, sweep_in = wavfile.read(args.sweep)
         record_rate, record_in = wavfile.read(args.record)
 
+        if sweep_rate != record_rate:
+            raise ValueError("Sample rates do not match, IR cannot be generated")
+
+        w1 = 20
+        w2 = sweep_rate / 2.0
+
         sweep_length = int(sweep_in.size)
-        k_end = 10**((-6 * np.log2(args.w2 / args.w1)) / 20)
+        k_end = 10**((-6 * np.log2(w2 / w1)) / 20)
         k = np.log(k_end) / sweep_length
 
         attenuation = np.exp(np.arange(sweep_length) * k)
@@ -48,7 +44,8 @@ class ProcessCommand(BaseCommand):
 
         if len(np.shape(record_in)) == 1:
             impulse_out = sig.wiener(sig.fftconvolve(record_in, divisor))
-            wavfile.write(args.output)
+            sample_max = np.max(np.abs(impulse_out))
+            wavfile.write(args.output, sweep_rate, np.float32(impulse_out / sample_max))
             return 0
         elif len(np.shape(record_in)) == 2:
             reshaped_record = np.array(list(zip(*record_in)))
